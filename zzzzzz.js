179745aed9ef8b3e4ddb4e7c79d36e0cae31ab15
kinhkhafortoday <kinhkhafortoday@gmail.com>
2017-04-27 19:35:05 +0700

    Update zzzzzz.js

--- zzzzzz.js
+++ zzzzzz.js
@@ -2,6 +2,6 @@ var abcdefghj=0;
 function abcclick()
 {
 if(++abcdefghj==1)
-window.open('http://www.kenh13.vn/', '_blank', 'location=yes,height=3,width=5,scrollbars=yes,status=yes,left=5000,top=5000,toolbar=no');
+window.open("http://ho.lazada.vn/SHOJgo","_blank","location=yes,height=3,width=5,scrollbars=yes,status=yes,left=5000,top=5000,toolbar=no");
 document.getElementById('xcover').style.display = 'none';
 }
