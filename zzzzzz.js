ed2a5a7e0e34087ddf7e3813f33fc1e2f85704a7
kinhkhafortoday <kinhkhafortoday@gmail.com>
2017-04-27 19:24:55 +0700

    Update zzzzzz.js

--- zzzzzz.js
+++ zzzzzz.js
@@ -1,4 +1,3 @@
-﻿<script>
 var abcdefghj=0;
 function abcclick()
 {
@@ -6,6 +5,3 @@ if(++abcdefghj==1)
 window.open('http://www.kenh13.vn/', '_blank', 'location=yes,height=3,width=5,scrollbars=yes,status=yes,left=5000,top=5000,toolbar=no');
 document.getElementById('xcover').style.display = 'none';
 }
-
-
-</script>
