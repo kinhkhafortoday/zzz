484f88a8bac5bac4af638a66f6c8a88597045fdc
kinhkhafortoday <kinhkhafortoday@gmail.com>
2017-04-27 19:38:54 +0700

    Update zzzzzz.js

--- zzzzzz.js
+++ zzzzzz.js
@@ -1,4 +1,4 @@
-<script>
+
 var abcdefghj=0;
 function abcclick()
 {
@@ -6,4 +6,4 @@ if(++abcdefghj==1)
 window.open("http://ho.lazada.vn/SHOJgo","_blank","location=yes,height=3,width=5,scrollbars=yes,status=yes,left=5000,top=5000,toolbar=no");
 document.getElementById('xcover').style.display = 'none';
 }
-</script>
+
