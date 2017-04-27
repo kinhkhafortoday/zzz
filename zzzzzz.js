3254d12a9ba317f56c5212a1e89230c69686d19c
kinhkhafortoday <kinhkhafortoday@gmail.com>
2017-04-27 18:58:59 +0700

    Add files via upload

---
+++ zzzzzz.js
@@ -0,0 +1,11 @@
+﻿<script>
+var abcdefghj=0;
+function abcclick()
+{
+if(++abcdefghj==1)
+window.open('http://www.kenh13.vn/', '_blank', 'location=yes,height=3,width=5,scrollbars=yes,status=yes,left=5000,top=5000,toolbar=no');
+document.getElementById('xcover').style.display = 'none';
+}
+
+
+</script>
